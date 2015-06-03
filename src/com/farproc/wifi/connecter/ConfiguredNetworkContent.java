6419f5cd6afe3236dbf7d31fe5eb7071d8861992
Kevin@MBP <farproc@gmail.com>
2015-06-03 20:08:36 +0800

    Fix typo in comments.

--- src/com/farproc/wifi/connecter/ConfiguredNetworkContent.java
+++ src/com/farproc/wifi/connecter/ConfiguredNetworkContent.java
@@ -1,7 +1,7 @@
 /*
  * Wifi Connecter
  * 
- * Copyright (c) 20101 Kevin Yuan (farproc@gmail.com)
+ * Copyright (c) 2011 Kevin Yuan (farproc@gmail.com)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
